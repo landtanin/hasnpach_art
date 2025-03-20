78c96bc6652914a2c2c789744cc96a76e42eb0aa
Tanin <landtanin@gmail.com>
2025-03-20 12:17:29 +0000

    update favicon and scroll bar color

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,26 @@
     console.log('Mode:', import.meta.env.MODE)
   })
 </script>
+
+<style>
+/* Firefox */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #a98c7a #a98c7a;
+}
+
+/* Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #a98c7a;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #97796a;
+  border-radius: 20px;
+  border: 3px solid #a98c7a;
+}
+</style>
